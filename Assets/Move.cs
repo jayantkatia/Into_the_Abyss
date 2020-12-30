5671acea4def57a17b7005fdb60f82e00d5c952d
= <=>
2020-12-30 23:26:01 +0530

    almost complete

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Move : MonoBehaviour
 {
     // Start is called before the first frame update
+    AudioSource aud;
     void Start()
     {
-        
+        aud = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -17,6 +19,14 @@ public class Move : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             transform.position += Camera.main.transform.up * Time.deltaTime * 15f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "stairs")
+        {
+            aud.Play();
         }
     }
 }
---
+++ Assets/sceneSwitching.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSwitching : MonoBehaviour
+{
+    public void ButtonStart()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ButtonCredit()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
